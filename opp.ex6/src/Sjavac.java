3e9f8141c9413938c83996a4f88b03a8541832aa
raizel.g <rochelleah>
2023-01-15 18:07:53 +0200

    added constructor

--- opp.ex6/src/Sjavac.java
+++ opp.ex6/src/Sjavac.java
@@ -1,4 +1,19 @@
-public class Sjavac {
+import java.io.*;
+
+public class Sjavac  {
+    public Sjavac(String fileName) throws FileNotFoundException {
+        try (InputStream inputFile = new FileInputStream(fileName)){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputFile));
+            String line;
+            while ((line = reader.readLine())!=null){
+
+            }
+        }catch (IOException e){
+            System.err.println("Invalid file name");
+
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("test");
     }
