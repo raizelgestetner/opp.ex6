c84895e6a8da0c9f49ceeca4fbab9c041fcd45b3
shira_p <shira.parker@mail.huji.ac.il>
2023-01-17 14:08:10 +0200

    update factory

--- opp.ex6/src/type_checker/VariableTypeCheckerFactory.java
+++ opp.ex6/src/type_checker/VariableTypeCheckerFactory.java
@@ -7,27 +7,27 @@ import com.sun.jdi.InvalidTypeException;
  */
 public class VariableTypeCheckerFactory {
 
-    public static final String STRING = "String";
-    private static final String INT = "int";
-    private static final String DOUBLE = "double";
-    public static final String BOOLEAN = "boolean";
-    public static final String CHAR = "char";
+    public static final String STRING_TYPE = "String";
+    private static final String INT_TYPE = "int";
+    private static final String DOUBLE_TYPE = "double";
+    public static final String BOOLEAN_TYPE = "boolean";
+    public static final String CHAR_TYPE = "char";
     public static final String VOID_TYPE = "void";
 
 
     public TypeChecker getType(String variableType, String line,int scopeLevel,String name )
             throws InvalidTypeException {
          switch (variableType) {
-             case INT:
+             case INT_TYPE:
                  return new IntTypeChecker(line);
-             case DOUBLE:
+             case DOUBLE_TYPE:
                  return new DoubleTypeChecker(line);
-             case STRING :
+             case STRING_TYPE :
                  return new StringTypeChecker(line);
-             case BOOLEAN:
+             case BOOLEAN_TYPE:
 
                  return new BooleanTypeChecker(line);
-             case CHAR:
+             case CHAR_TYPE:
                  return new CharTypeChecker(line);
              case VOID_TYPE:
                  return new MethodChecker(line,scopeLevel,name);
